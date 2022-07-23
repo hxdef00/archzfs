38944756f8ceab0d458e3db6c0225b9890a696d4
hxdef00 <109024684+hxdef00@users.noreply.github.com>
2022-07-23 16:38:51 +0200

    Create partition.sh

---
+++ partition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ![[ -z "${DISK}" ]]; then
+  echo 'define DISK'
+  exit
+fi
+
+INST_LINVAR='linux'
+INST_UUID=$(dd if=/dev/urandom bs=1 count=100 2>/dev/null | tr -dc 'a-z0-9' | cut -c-6)
+INST_ID=arch
+INST_PRIMARY_DISK=$(echo $DISK | cut -f1 -d\ )
