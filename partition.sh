51eb91336f0bfa09eecf786b62552cffe3e162a1
hxdef00 <109024684+hxdef00@users.noreply.github.com>
2022-07-23 22:42:50 +0200

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -5,15 +5,6 @@ if [[ -z "${DISK}" ]]; then
   exit
 fi
 
-export INST_LINVAR='linux'
-export INST_UUID=$(dd if=/dev/urandom bs=1 count=100 2>/dev/null | tr -dc 'a-z0-9' | cut -c-6)
-export INST_ID=arch
-export INST_PRIMARY_DISK=$(echo $DISK | cut -f1 -d\ )
-
-export INST_PARTSIZE_ESP=4
-export INST_PARTSIZE_BPOOL=4
-export INST_PARTSIZE_SWAP=32
-
 for i in ${DISK}; do
   blkdiscard -f $i &
 done
